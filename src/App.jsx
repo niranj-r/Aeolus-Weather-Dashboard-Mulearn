99410fdfa1ac3d04ba3fd9400b02fa8863ddef51
niranj-r <niranj.njai@gmail.com>
2025-08-03 23:19:59 +0530

    Add useCallback to imports in App component for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from './App.module.css';
 import WeatherSearch from './components/WeatherSearch';
 import WeatherCard from './components/WeatherCard';
